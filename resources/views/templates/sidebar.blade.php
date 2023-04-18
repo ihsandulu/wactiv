d13d2e62efa0e9badd6829f93560656f78f43bbd
ibadi ichsan <ihsan.dulu@gmail.com>
2023-04-18 17:42:39 +0700

    perbaikan menu mcategory

--- resources/views/templates/sidebar.blade.php
+++ resources/views/templates/sidebar.blade.php
@@ -3,6 +3,7 @@
     <div class="sidebar-heading border-bottom bg-light"><img src="images/tokoqit_logo.png" style="width:80px; height:inherit;"/></div>
     <div class="list-group list-group-flush">
         @auth
+            <!-- Admin -->
             <?php if(auth()->user()->position_id==1){?>
                 <a class="list-group-item list-group-item-action list-group-item-light p-3 bg-secondary text-white disabled" href="#"> Admin </a>
                 <!-- <a class="list-group-item list-group-item-action list-group-item-light p-3" href="{{ url('/category?default=OK') }}"> Category </a>
@@ -10,18 +11,24 @@
                 <a class="list-group-item list-group-item-action list-group-item-light p-3" href="{{ url('/layanans') }}">Layanan</a>
                 <!-- <a class="list-group-item list-group-item-action list-group-item-light p-3" href="{{ url('/transaction?default=OK') }}">Transaksi</a> -->
             <?php }?>
-            <a class="list-group-item list-group-item-action list-group-item-light p-3 bg-secondary text-white disabled" href="#"> Layanan Customer </a>        
+            
+            
+             <!-- Layanan -->
+            <?php if(isset($_GET["layananid"])){?>
+                <a class="list-group-item list-group-item-action list-group-item-light p-3 bg-secondary text-white disabled" href="#"> Layanan Customer </a>
+                <a class="list-group-item list-group-item-action list-group-item-light p-3" href="<?= url("/mcategory?default=OK&layananid=".$_GET["layananid"]."&layananname=".$_GET["layananname"]) ;?>"> Kategori Member </a>
+            <?php }?>  
         
-            <a class="list-group-item list-group-item-action list-group-item-light p-3" href="{{ url('/mcategory') }}">Kategori Member</a>
+        @endauth
+            
+        
+        <a class="list-group-item list-group-item-action list-group-item-light p-3 bg-secondary text-white disabled" href="#"> Umum </a> 
+        @endauth
+            <a class="list-group-item list-group-item-action list-group-item-light p-3" href="{{ url('/password') }}">Password</a>
             <a class="list-group-item list-group-item-action list-group-item-light p-3" href="{{ url('/layanan') }}">Layanan</a>
             <!-- <a class="list-group-item list-group-item-action list-group-item-light p-3" href="{{ url('/transactions') }}">Transaksi</a>
             <a class="list-group-item list-group-item-action list-group-item-light p-3" href="{{ url('/layanan?expired=OK') }}"> Tagihan </a> -->
             <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#">{{ auth()->user()->user_name}}</a>
-        @endauth
-            
-        
-        @endauth
-            <a class="list-group-item list-group-item-action list-group-item-light p-3" href="{{ url('/password') }}">Password</a>
         @else                    
             <a class="list-group-item list-group-item-action list-group-item-light p-3" href="{{ url('/berita') }}">Berita</a>
         @endauth
